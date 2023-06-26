d237f728c614313a711d6301fad4a15e054ab6cc
lunxnixl <129772776+lunxnixl@users.noreply.github.com>
2023-06-26 21:07:14 +0200

    Add files via upload

---
+++ terre04.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import sys
+
+num = str(sys.argv[1])
+
+alphabet = ("abcdefghijklmnopqrstuvwxyz")
+
+
+if num == alphabet[0]:
+    print(alphabet[:26])
+elif num == alphabet[1]:
+    print(alphabet[1:])
+elif num == alphabet[2]:
+    print(alphabet[2:])
+elif num == alphabet[3]:
+    print(alphabet[3:])
+elif num == alphabet[4]:
+    print(alphabet[4:])
+elif num == alphabet[5]:
+    print(alphabet[5:])
+elif num == alphabet[6]:
+    print(alphabet[6:])
+elif num == alphabet[7]:
+    print(alphabet[7:])
+elif num == alphabet[8]:
+    print(alphabet[8:])
+elif num == alphabet[9]:
+    print(alphabet[9:])
+elif num == alphabet[10]:
+    print(alphabet[10:])
+elif num == alphabet[11]:
+    print(alphabet[11:])
+elif num == alphabet[12]:
+    print(alphabet[12:])
+elif num == alphabet[13]:
+    print(alphabet[13:])
+elif num == alphabet[14]:
+    print(alphabet[14:])
+elif num == alphabet[13]:
+    print(alphabet[15:])
+elif num == alphabet[16]:
+    print(alphabet[16:])
+elif num == alphabet[17]:
+    print(alphabet[17:])
+elif num == alphabet[18]:
+    print(alphabet[18:])
+elif num == alphabet[19]:
+    print(alphabet[19:])
+elif num == alphabet[20]:
+    print(alphabet[20:])
+elif num == alphabet[21]:
+    print(alphabet[21:])
+elif num == alphabet[22]:
+    print(alphabet[22:])
+elif num == alphabet[23]:
+    print(alphabet[23:])
+elif num == alphabet[24]:
+    print(alphabet[24:])
+elif num == alphabet[25]:
+    print(alphabet[25:])
